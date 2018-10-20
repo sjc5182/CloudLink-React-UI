f015616ba90f65d8efa41ddea9545eaeea35d9da
John Chen <shiguangchen1987@gmail.com>
2018-10-20 01:19:54 -0700

    Create index.js

---
+++ src/components/sale-order-component/style/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { font, color, grid } from '../../../globals/'
+
+export const SalesOrderComponentStyle = styled.div`
+  margin-top: ${grid.scale(2)};
+  display: flex;
+  flex-direction: column;
+  .sales-order-main-component-wrapper{
+    display:flex;
+  }
+`
